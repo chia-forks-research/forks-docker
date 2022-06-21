3032f08b8bc6b18154fee0f3f7df8cefc3e73dbc
Javanaut <javanaut@refulgent.de>
2022-06-21 15:08:58 +0200

    Re-add Forks Repos

---
+++ src/chives-docker/docker-start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2154,SC2086
+chives start ${service}
+
+trap "echo Shutting down ...; chives stop all -d; exit 0" SIGINT SIGTERM
+
+# shellcheck disable=SC2154
+if [[ ${log_to_file} == 'true' ]]; then
+  # Ensures the log file actually exists, so we can tail successfully
+  touch "$CHIVES_ROOT/log/debug.log"
+  tail -F "$CHIVES_ROOT/log/debug.log" &
+fi
+
+while true; do sleep 1; done
